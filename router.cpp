d3b0498cdbe82247caed64e1392978780c46f0ff
phanhuy1502 <phanhuy1502@gmail.com>
2016-04-06 15:52:38 -0400

    add tree node structure and update routes.txt

--- router.cpp
+++ router.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include <string.h>   
+#include <stdlib.h> 
 
 using namespace std;
 
+struct node
+{
+  string ip_address_destination;
+  string gateway;
+  string interface;
+  struct node *left;
+  struct node *right;
+};
+
 int main() {
+	struct node *root;
+	string address_binary = "00000001001000010001000101100001";
+	for 
 	cout << "our main source code file";
 }
