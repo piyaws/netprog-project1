580548177bd55b259f62a35e95d38c5f9f5b86f2
phanhuy1502 <phanhuy1502@gmail.com>
2016-04-06 17:04:27 -0400

    add code structure

--- router.cpp
+++ router.cpp
@@ -12,10 +12,16 @@ struct node
   struct node *right;
 };
 
+/* @Yu: mac address lookup 
+	put your code here
+*/
+string getMAC(string gateway) {
+	return "aa:bb:cc:dd:ee:ff";
+}
+
 int main() {
 	
 	// 1. BUILDING THE BINARY TREE FOR THE ROUTING
-
 	struct node *root; // root of our tree
 	root = (struct node*) malloc( sizeof( struct node ) );
 	struct node *cur; // use to traverse
@@ -49,17 +55,20 @@ int main() {
 	cur->interface = new string(interface);
 	cur = root;
 	cout << "our main source code file" << endl;
-}
 
-/* Fill BST with empty node at current height
-void fill_tree(struct node *root, int height){
-	count++;
-	if (count%10000 == 0) {
-		cout << count << endl;
-	}
-	if (height == 32) return;
-	root->left = (struct node*) malloc( sizeof( struct node ) );
-	root->right = (struct node*) malloc( sizeof( struct node ) );
-	fill_tree(root->left, height +1);
-	fill_tree(root->right, height+1);
-}*/
+	// 2. PARSING ARP.TXT AND CREATE A MAP TO LOOKUP MAP ADDRESS
+	// @Yu: your code here
+
+	// 3. PARSING PDU.TXT AND ROUTE
+
+	// 3.1. Parse pdu.txt
+
+	// 3.2. route
+	string source_addr = "1.2.3.4";
+	string source_port = "11";
+	string dest_addr = "5.6.7.8";
+	string dest_addr_bin = "00000101000001100000011100001000";
+	string dest_port = "22";
+	int ttl = 11;
+	
+}
