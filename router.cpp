d0d269ac1576ab7b01fb922888fe9d2af5ae473b
phanhuy1502 <phanhuy1502@gmail.com>
2016-04-07 02:54:34 -0400

    integrate parsing and building tree

--- router.cpp
+++ router.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
-#include <string.h>   
-#include <stdlib.h> 
+#include <string.h>
+#include <stdlib.h>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <cstdint>
+#include <bitset>
+#include <typeinfo>
+
+const int MAX_CHARS_PER_LINE = 512;
+const int MAX_TOKENS_PER_LINE = 20;
+const char* const DELIMITER = " /";
 
 using namespace std;
 
@@ -12,7 +22,7 @@ struct node
   struct node *right;
 };
 
-/* @Yu: mac address lookup 
+/* @Yu: mac address lookup
 	put your code here
 */
 string getMAC(string gateway) {
@@ -20,43 +30,96 @@ string getMAC(string gateway) {
 }
 
 int main() {
-	
+
 	// 1. BUILDING THE BINARY TREE FOR THE ROUTING
 
 	struct node *root; // root of our tree
 	root = (struct node*) malloc( sizeof( struct node ) );
 	struct node *cur; // use to traverse
 
-	string address_binary = "00110001001000010001000101100001";
-	int cidr = 7;
-	string gateway = "1.2.3.4";
-	string interface = "eth1";
-	cur = root;
+	// reading routes.txt
+  	ifstream fin;
+  	fin.open("routes.txt");
+  	if (!fin.good())
+    	return 1;
 
-	// loop for each line of routes.txt
-	for (int i = 0 ; i < cidr; i++) {
-        if (address_binary[i] == '0') {
-        	if (cur->left == NULL) {
-        		cur->left = (struct node*) malloc( sizeof( struct node ) );
-        	}
-    		cout << "Go left" << endl;
-			cur = cur->left;
-        }
-        else {
-        	if (cur->right == NULL) {
-        		cur->right = (struct node*) malloc( sizeof( struct node ) );
-        	}
-        	cout << "Go right" << endl;
-        	cur = cur->right;
-        }
+    // PARSING destination cidr gateway interface
+    string destination, cidr, gateway, interface;
+
+	// read each line of the file
+  	while (!fin.eof()) {
+    	// read an entire line into memory
+	    char buf[MAX_CHARS_PER_LINE];
+	    fin.getline(buf, MAX_CHARS_PER_LINE);
+
+	    // parse the line into blank-delimited tokens
+	    int n = 0; // a for-loop index
+
+	    // array to store memory addresses of the tokens in buf
+	    const char* token[MAX_TOKENS_PER_LINE] = {}; // initialize to 0
+
+	    // parse the line
+	    token[0] = strtok(buf, DELIMITER); // first token
+	    if (token[0]) { // zero if line is blank
+	      	for (n = 1; n < MAX_TOKENS_PER_LINE; n++) {
+	        	token[n] = strtok(0, DELIMITER); // subsequent tokens
+	        	if (!token[n]) break; // no more tokens
+	      	}
+	    }
+
+	    destination = token[0];
+	    cidr = token[1];
+	    gateway = token[2];
+	    interface = token[3];
+
+	    int bit;
+	    char *pch;
+	    string ip_addr_in_bit = "";
+	    pch = strtok((char*)token[0],".");
+	    while (pch != NULL) {
+	    	//cout << pch << endl;
+	    	bit = atoi(pch);
+	    	bitset<8> bin(bit);
+	    	ip_addr_in_bit += bin.to_string();
+
+	    	pch = strtok(NULL,".");
+	    }
+
+	    /*cout << "IP address of the destination in binary: " << ip_addr_in_bit << endl;
+	    cout << "destination = " << destination << endl;
+	    cout << "cidr = " << cidr << endl;
+	    cout << "gateway = " << gateway << endl;
+	    cout << "interface = " << interface << endl;*/
+
+		int cidr = 7;
+		cur = root;
+
+		// loop for each line of routes.txt
+		for (int i = 0 ; i < cidr; i++) {
+	        if (ip_addr_in_bit[i] == '0') {
+	        	if (cur->left == NULL) {
+	        		cur->left = (struct node*) malloc( sizeof( struct node ) );
+	        		cur->left->left = NULL;
+                    cur->left->right = NULL;
+	        	}
+				cur = cur->left;
+	        }
+	        else {
+	        	if (cur->right == NULL) {
+	        		cur->right = (struct node*) malloc( sizeof( struct node ) );
+	        		cur->right->left = NULL;
+                    cur->right->right = NULL;
+	        	}
+	        	cur = cur->right;
+	        }
+		}
+
+		cur->gateway = new string(gateway);
+		cur->interface = new string(interface);
+		cur = root;
+        cout << "Done for this entry" << endl;
 	}
-	if (cur == NULL) {
-		cur = (struct node*) malloc( sizeof( struct node ) );
-	}
-	cur->gateway = new string(gateway);
-	cur->interface = new string(interface);
-	cur = root;
-	cout << "our main source code file" << endl;
+	fin.close();
 
 	// 2. PARSING ARP.TXT AND CREATE A MAP TO LOOKUP MAP ADDRESS
 	// @Yu: your code here
